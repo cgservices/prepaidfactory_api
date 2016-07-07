f7200ec0cadf7615d1501e8b19a687d33a5bdf06
Sander van Rossum <sander.van.rossumg@cg.nl>
2016-07-07 16:10:07 +0200

    Added :status to order entity

--- lib/prepaidfactory_api/client/entities/order.rb
+++ lib/prepaidfactory_api/client/entities/order.rb
@@ -3,7 +3,7 @@ module PrepaidfactoryApi
   module Entities
     class Order < Entities::Base
 
-      attr_reader :order_id, :terminal_id, :reference, :order_date, :currency, :provider_name, :product_id, :eancode, :promotion_text, :activation_instructions, :vouchers
+      attr_reader :order_id, :terminal_id, :reference, :order_date, :currency, :provider_name, :product_id, :eancode, :promotion_text, :activation_instructions, :vouchers, :status
 
       def initialize(order)
         @order_id = order[:order_id]
@@ -17,6 +17,7 @@ module PrepaidfactoryApi
         @promotion_text = order[:promotion_text]
         @activation_instructions = order[:activation_instructions]
         @vouchers = order[:vouchers]
+        @status = order[:status]
       end
 
     end
