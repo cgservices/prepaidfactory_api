dc966a7c62f5ac4ed6246559102343215986d63a
Sander van Rossum <sander.van.rossumg@cg.nl>
2016-07-19 12:58:46 +0200

    Updated example.rb

--- example.rb
+++ example.rb
@@ -8,36 +8,46 @@ config = YAML.load(File.open('./config/ppf_config.yml'))
 client = PrepaidfactoryApi::Client.new(config)
 
 # Retrieve products
+puts "\n=============================================================="
+puts ' RETRIEVE PRODUCTS'
+puts '=============================================================='
 request = PrepaidfactoryApi::Requests::GetProductInformation.new(config['retailer_id'])
 products = client.get_product_information(request)
 
 product_id = ''
 products.each { |product|
   puts "#{product.product_id}: #{product.description}"
-  # product_id = product.product_id
-  # product.each { |key, value|
-  #  puts "#{key.to_s.delete('@').ljust(25)}: #{product.instance_variable_get(key)}"
-  # }
-  # puts "========================================================================="
+  product_id = product.product_id
+  product.each { |key, value|
+    puts "#{key.to_s.ljust(25)}: #{value}"
+  }
+  puts "\n=============================================================="
 }
 
 # Create order
-puts "\n==============================================================\nCREATING ORDER FOR #{product_id}\n=============================================================="
-request = PrepaidfactoryApi::Requests::CreateOrder.new(config['retailer_id'], 'C9910', 'TEST-TERMINAL')
+puts "\n=============================================================="
+puts " CREATING ORDER FOR #{product_id}"
+puts '=============================================================='
+# product_id = 'SSC50'
+request = PrepaidfactoryApi::Requests::CreateOrder.new(config['retailer_id'], product_id, 'TEST-TERMINAL')
 order = client.create_order(request)
 order.each { |key, value|
-  puts "#{key.to_s.delete('@').ljust(25)}: #{order.instance_variable_get(key)}"
+  puts "#{key.to_s.ljust(25)}: #{value}"
 }
 
 # Cancel order
-# puts "\n==============================================================\nCANCEL ORDER\n=============================================================="
+# puts "\n=============================================================="
+# puts "CANCEL ORDER #{product_id}"
+# puts '=============================================================='
 # request = PrepaidfactoryApi::Requests::CancelOrder.new(order.order_id)
 # p client.cancel_order(request)
 
 # Confirm order
-# puts "\n==============================================================\nCONFIRM ORDER #{order.order_id}\n=============================================================="
-# request = PrepaidfactoryApi::Requests::ConfirmOrder.new(order.order_id)
-# order = client.confirm_order(request)
-# order.each { |key, value|
-#   puts "#{key.to_s.delete('@').ljust(25)}: #{order.instance_variable_get(key)}"
-# }
+puts "\n=============================================================="
+puts "CONFIRM ORDER #{order.order_id}"
+puts '=============================================================='
+request = PrepaidfactoryApi::Requests::ConfirmOrder.new(order.order_id)
+order = client.confirm_order(request)
+order.each { |key, value|
+  puts "#{key.to_s.ljust(25)}: #{value}"
+}
