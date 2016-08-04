f9e26b57fd89c1c941a483e4989b8f6b9f394351
Sander van Rossum <sander.van.rossumg@cg.nl>
2016-08-04 09:19:24 +0200

    Fix for DCGW

--- lib/prepaidfactory_api/entities/order.rb
+++ lib/prepaidfactory_api/entities/order.rb
@@ -15,7 +15,7 @@ module PrepaidfactoryApi
         @eancode = order[:eancode]
         @promotion_text = order[:promotion_text]
         @activation_instructions = order[:activation_instructions]
-        @vouchers = order[:consumer_service_vouchers].values
+        @vouchers = order[:consumer_service_vouchers].values if order[:consumer_service_vouchers].respond_to?(:values)
         @status = order[:status]
       end
     end
