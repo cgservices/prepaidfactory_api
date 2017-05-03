50a26e25e0687f678d862cf56ff8a500d65f0dce
Ralf Claassens <ralf.claassens@cg.nl>
2017-05-03 11:54:14 +0200

    Disables cancelable flag (according to changed requirement from PPF)

--- lib/prepaidfactory_api/requests/create_order.rb
+++ lib/prepaidfactory_api/requests/create_order.rb
@@ -2,7 +2,7 @@ module PrepaidfactoryApi
   module Requests
     # CreateOrder request class
     class CreateOrder < Requests::Base
-      def initialize(retailer_id, product_id, terminal_id, nr_of_vouchers=1, reference='', is_cancelable=true, auto_confirm=true)
+      def initialize(retailer_id, product_id, terminal_id, nr_of_vouchers=1, reference='', is_cancelable=false, auto_confirm=true)
         @RetailerID = retailer_id
         @ProductID = product_id
         @TerminalID = terminal_id
