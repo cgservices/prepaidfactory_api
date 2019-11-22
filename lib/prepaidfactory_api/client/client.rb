f3dcdbb89a3bc2b8cebb8dc1e738fe30c5476222
Nicolas Dreux <nclsdrx@gmail.com>
2019-11-22 11:31:39 +0100

    Decrease the default open_timeout
    Change the default open_timeout from 10 to 5

--- lib/prepaidfactory_api/client/client.rb
+++ lib/prepaidfactory_api/client/client.rb
@@ -15,7 +15,7 @@ module PrepaidfactoryApi
   # This is the Prepaid Factory API client
   class Client
     def initialize(config)
-      config['open_timeout'] ||= 10
+      config['open_timeout'] ||= 5
       config['read_timeout'] ||= 65
       config['ssl_verify_mode'] ||= :peer
       config['ssl_ca_cert_file'] ||= config['pem_cert']
